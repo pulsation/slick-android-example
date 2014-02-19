6f86623950a941b0acd73bd614b4fd922f15cd4b
pulsation <philippe.sam-long@laposte.net>
2014-02-19 15:52:24 +0100

    Display saved data

--- src/eu/pulsation/slicksandbox/SlickSandbox.scala
+++ src/eu/pulsation/slicksandbox/SlickSandbox.scala
@@ -1,17 +1,11 @@
-
-// Slick example: https://stackoverflow.com/questions/21314532/slick-n-scala-a-tablequery-object-without-ddl-field
-// https://github.com/slick/slick-examples/blob/master/src/main/scala/com/typesafe/slick/examples/lifted/FirstExample.scala
-// doc: http://slick.typesafe.com/doc/0.11.1/gettingstarted.html
-
 package eu.pulsation.slicksandbox
 
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.{EditText, TextView}
 import scala.slick.driver.SQLiteDriver.simple._
 import scala.slick.jdbc.meta.MTable
-import android.util.Log
 
 class SlickSandbox extends Activity
 {
@@ -23,32 +17,52 @@ class SlickSandbox extends Activity
     def * = (id, name)
   }
   val suppliers = TableQuery[Suppliers]
-  lazy val db = Database.forURL("jdbc:sqlite:" + getApplicationContext().getFilesDir() + "slick-sandbox.txt", driver = "org.sqldroid.SQLDroidDriver")
+  lazy val db = Database.forURL("jdbc:sqlite:" +
+    getApplicationContext().getFilesDir() +
+    "slick-sandbox.txt", driver = "org.sqldroid.SQLDroidDriver")
 
-    /** Called when the activity is first created. */
-    override def onCreate(savedInstanceState : Bundle)
-    {
-        super.onCreate(savedInstanceState)
-      setContentView(R.layout.main)
-      db withSession {
-        implicit session =>
-        // Create table if needed
-          if (MTable.getTables("NAMES").list().isEmpty) {
-            (suppliers.ddl).create
-          }
+  // Views
+  lazy val mText : TextView = findViewById(R.id.text) match { case t : TextView => t }
+  lazy val mEdit : EditText = findViewById(R.id.data) match { case e : EditText => e }
 
-          // Get existing rows
-          suppliers.list.foreach(supplier => android.util.Log.v("DEBUG", supplier._1 + " " + supplier._2))
-      }
+  def createDb() = {
+    db withSession {
+      implicit session =>
+      // Create table if needed
+        if (MTable.getTables("NAMES").list().isEmpty) {
+          (suppliers.ddl).create
+        }
     }
+  }
 
-   def saveDataText(view : View) {
-   Toast.makeText(getApplicationContext(), "TODO", Toast.LENGTH_LONG).show()
-     db withSession {
-       implicit session =>
-         //suppliers += ("Test");
-         suppliers.insert(0, "Test")
-     }
-	// TODO
-   }
+  def displayRows() = {
+    mText.setText("")
+    db withSession {
+      implicit session =>
+      // Get existing rows
+        suppliers.list.foreach(
+          supplier =>
+            mText.append(supplier._1 +
+              " " + supplier._2 +
+              System.getProperty("line.separator"))
+        )
+    }
+  }
+
+  /** Called when the activity is first created. */
+  override def onCreate(savedInstanceState : Bundle)
+  {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+    createDb()
+    displayRows()
+  }
+
+  def saveDataText(view : View) {
+    db withSession {
+      implicit session =>
+        suppliers += (0, mEdit.getText().toString)
+    }
+    displayRows()
+  }
 }
