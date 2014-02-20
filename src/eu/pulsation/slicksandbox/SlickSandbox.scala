00fd0cd7680f3d1045a5c1eacef6dbab785cc5a3
pulsation <philippe.sam-long@laposte.net>
2014-02-20 12:04:58 +0100

    Comments

--- src/eu/pulsation/slicksandbox/SlickSandbox.scala
+++ src/eu/pulsation/slicksandbox/SlickSandbox.scala
@@ -3,13 +3,14 @@ package eu.pulsation.slicksandbox
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
-import android.widget.{EditText, TextView}
+import android.widget.{Toast, EditText, TextView}
 
 import scala.slick.driver.SQLiteDriver.simple._
 import scala.slick.jdbc.meta.MTable
 
 import scala.concurrent._
 import ExecutionContext.Implicits.global
+import scala.util.Success
 
 class SlickSandbox extends Activity
 {
@@ -18,13 +19,18 @@ class SlickSandbox extends Activity
   // Implicit conversion to Runnable when called by runOnUiThread()
   implicit def toRunnable[F](f: => F): Runnable = new Runnable() { def run() = f }
 
+  // Table definition
   class MyData(tag: Tag) extends Table[(Int, String)](tag, TableName) {
     def id = column[Int]("ID", O.PrimaryKey, O.AutoInc) // This is the primary key column
     def name = column[String]("NAME")
     // Every table needs a * projection with the same type as the table's type parameter
     def * = (id, name)
   }
+
+  // Table instance
   val myData = TableQuery[MyData]
+
+  // Database connection
   lazy val db = Database.forURL("jdbc:sqlite:" +
     getApplicationContext().getFilesDir() +
     "slick-sandbox.txt", driver = "org.sqldroid.SQLDroidDriver")
@@ -33,16 +39,22 @@ class SlickSandbox extends Activity
   lazy val mText : TextView = findViewById(R.id.text) match { case t : TextView => t }
   lazy val mEdit : EditText = findViewById(R.id.data) match { case e : EditText => e }
 
-  def createDb() = {
+  /**
+   * Create the table if needed
+   */
+  def createTable() = {
     db withSession { implicit session =>
-      // Create table if needed
       if (MTable.getTables(TableName).list().isEmpty) {
         (myData.ddl).create
       }
     }
   }
 
-  def getRows() = {
+  /**
+   * Fetch all rows
+   * @return rows
+   */
+  def fetchRows() = {
     db withSession {
       implicit session =>
       // Get existing rows
@@ -50,6 +62,10 @@ class SlickSandbox extends Activity
     }
   }
 
+  /**
+   * Display data in text view
+   * @param rows Data to display
+   */
   def displayDataList(rows : List[(Int,String)]) = {
     mText.setText("")
     rows foreach({ case (id : Int, name: String) =>
@@ -59,35 +75,56 @@ class SlickSandbox extends Activity
     })
   }
 
-  def saveData() = {
+  /**
+   * Add one row to table
+   */
+  def saveData() : Unit = {
     db withSession {
       implicit session =>
         myData += (0, mEdit.getText().toString)
     }
   }
 
-  def processThenDisplay(process : () => Unit) {
+  /**
+   * Process data, then fetch all data to update the UI
+   * @param process
+   */
+  def processThenDisplay(process : () => Unit) : Future[List[(Int, String)]] = {
     val fProcessData = Future { process() }
     val fFetchData : Future[List[(Int, String)]] = fProcessData map((nothing) => {
-      // This will be executed after f1 has been executed
-      getRows()
+      // This will be executed after data has been processed
+      fetchRows()
     })
 
     fFetchData onSuccess {
       case rows =>
         runOnUiThread({ displayDataList(rows) })
     }
+    fFetchData
   }
 
-  /** Called when the activity is first created. */
+  // Initialize data
+  lazy val initFuture = processThenDisplay(createTable)
+
+  /**
+   * Called when the activity is first created.
+   */
   override def onCreate(savedInstanceState : Bundle)
   {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
-    processThenDisplay(createDb)
+
+    initFuture
   }
 
+  /**
+   * Insert edit text contents into database
+   */
   def saveDataText(view : View) {
-    processThenDisplay(saveData)
+    if (initFuture.isCompleted) {
+      processThenDisplay(saveData)
+    } else {
+      Toast.makeText(getApplicationContext(), "Database not initialized yet", Toast.LENGTH_SHORT).show()
+    }
   }
 }
